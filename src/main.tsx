61a2f454f7218d2e2939e7d8db4f56c40bc3ff7d
Terrin <923727421@qq.com>
2024-11-24 23:30:25 +0800

    feat: 添加classnames

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import './style/reset.less';
 import './index.css';
 import App from './App.tsx';
 import {Routes, Route} from 'react-router';
