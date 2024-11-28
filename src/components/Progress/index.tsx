f2a6c6919d41235ce658c74767901a1b588c113d
Terrin <923727421@qq.com>
2024-11-28 23:01:45 +0800

    feat: 完成进度条动画demo

--- src/components/Progress/index.tsx
+++ src/components/Progress/index.tsx
@@ -1,40 +1,63 @@
 import {useEffect, useRef, useState} from 'react';
 import styles from './index.module.less';
-function Progress() {
-  const list = [10, 20, 40, 60, 70, 100, 300];
-  const value = 100;
+
+type Props = {
+  index: number;
+  /** 默认显示几个 */
+  defaultShowCount?: number;
+  /** 进度条的值 */
+  value: number;
+  list: number[];
+};
+const defaultList = [10, 20, 40, 60, 70, 100, 300];
+function Progress({index, list = defaultList, value = 20, defaultShowCount = 2}: Props) {
+  // const list = [10, 20, 40];
+  const listLen = list.length;
   const el = useRef<null | HTMLDivElement>(null);
   const [width, setWidth] = useState(0);
   const [valueWidth, setValueWidth] = useState(0);
+  const showCount = Math.min(defaultShowCount + 1, listLen);
 
   useEffect(() => {
-    const r = (el.current?.clientWidth || 0) / 4;
+    const r = (el.current?.clientWidth || 0) / showCount;
     setWidth(r);
 
     if (value <= list[0]) {
-      const d = (value / list[0]) * r + 30;
-      console.log(d, '-----------dd-----------------------');
+      const d = (value / list[0]) * r;
       setValueWidth(d);
       return;
     }
-    console.log(r);
     const idx = list.findIndex(item => item >= value);
-    /** 30 元素自身宽度 */
     const aa = list[idx] - value;
     const bb = list[idx] - list[idx - 1];
     const c = (1 - aa / bb) * r;
-    const valueWidth = idx * r + 30 + c;
+    const valueWidth = idx * r + c;
     setValueWidth(valueWidth);
-    console.log(idx, list[idx], idx, aa, bb, c, '------------diff----------------------');
   }, []);
+
+  const offset = {
+    transform: `translateX(-${index * width}px)`,
+  };
   return (
     <div className={styles.progress} ref={el}>
-      <div className={styles.progressValue} style={{width: valueWidth + 'px'}}></div>
+      <div
+        className={styles.progressBg}
+        style={{
+          ...offset,
+          width: listLen * width,
+        }}></div>
+      <div className={styles.progressValue} style={{...offset, width: valueWidth + 'px'}}></div>
       <div className={styles.progressStep}>
-        {list.map((item, index) => {
+        {list.map((item, k) => {
           return (
-            <div key={item} style={{left: (index + 1) * width + 'px'}}>
-              {item}
+            <div
+              key={item}
+              style={{
+                /** 30 元素自身宽度 */
+                transform: `translate(-${index * width}px,-50%)`,
+                left: (k + 1) * width - 30 + 'px',
+              }}>
+              <div className={styles.text}>{item}</div>
             </div>
           );
         })}
--- src/page/demo/index.tsx
+++ src/page/demo/index.tsx
@@ -1,11 +1,57 @@
 import Progress from '@/components/Progress';
+import styles from './index.module.less';
+import {useEffect, useRef, useState} from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import type {Swiper as SwiperType} from 'swiper';
+import 'swiper/css';
 function Demo() {
+  const [idx, setIdx] = useState(0);
+  const list = [10, 20, 40, 60, 70, 100, 300];
+
+  const per = useRef<SwiperType | null>(null);
+  useEffect(() => {
+    per?.current?.slideTo(idx);
+  }, [idx]);
   return (
     <div>
-      Demo
-      <div style={{width: 500, margin: 100}}>
-        <Progress />
+      <div className={styles.sbox}>
+        <Swiper
+          onSwiper={swiper => (per.current = swiper)}
+          spaceBetween={50}
+          centeredSlides
+          slidesPerView={3}
+          onSlideChange={v => {
+            setIdx(v.activeIndex);
+          }}>
+          {list.map(item => {
+            return (
+              <SwiperSlide className={'swiper-slide'} key={item}>
+                <div>Slide {item}</div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
+
+      <div className={styles.box}>
+        <Progress value={25} index={idx} list={list} />
+      </div>
+      <button
+        onClick={() => {
+          setIdx(oldV => {
+            return oldV + 1;
+          });
+        }}>
+        left
+      </button>
+      <button
+        onClick={() => {
+          setIdx(oldV => {
+            return oldV - 1;
+          });
+        }}>
+        right
+      </button>
     </div>
   );
 }
