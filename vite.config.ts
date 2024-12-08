d22a3d4f9a688cb47d4cb20a64310868d355cb15
TerrinTian-Spotec <terrin.tian@spotec.ne>
2024-12-08 16:44:47 +0800

    feat: 不更新的包打包到一个文件中

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,15 @@ export default defineConfig({
       '@': path.join(__dirname, './src/'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
