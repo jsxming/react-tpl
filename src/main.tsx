572474e7e70e31391cff8d3b1185d9785694bce5
TerrinTian-Spotec <terrin.tian@spotec.ne>
2024-12-08 16:14:13 +0800

    feat: 使用hashRouter、修改打包后的访问路径

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import {Routes, Route, BrowserRouter} from 'react-router';
+import {Routes, Route, HashRouter} from 'react-router';
 import Demo from '@/page/demo';
 import './style/reset.less';
 import './style/themes/index.less';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route index element={<App />} />
         <Route path='demo' element={<Demo />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
