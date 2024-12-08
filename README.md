9258e1a7d8a54a07cbf86dc11292ed33829195c8
TerrinTian-Spotec <terrin.tian@spotec.ne>
2024-12-08 15:28:59 +0800

    feat: vite-plugin-svgr添加

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # React + TypeScript + Vite
+
+### svg引入使用
+
+[详见 vite-plugin-svgr](https://www.npmjs.com/package/vite-plugin-svgr)
+
+```tsx
+// 注意加?react后缀否则不能识别详见：
+import Logo from './logo.svg?react';
+<Logo />;
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.join(__dirname, './src/'),
-		},
-	},
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, './src/'),
+    },
+  },
 });
